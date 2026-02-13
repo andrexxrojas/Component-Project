b278492f7a8283938bedd62397bba6010863f847
andrexxrojas <jayandrerojas@gmail.com>
2026-02-13 15:17:26 +0800

    Allow user to create new project from top nav and update project grid

--- frontend/src/components/AuthenticatedNav/AuthenticatedNav.jsx
+++ frontend/src/components/AuthenticatedNav/AuthenticatedNav.jsx
@@ -2,11 +2,13 @@ import styles from "./AuthenticatedNav.module.css";
 import {MagnifyingGlassIcon, PlusIcon} from "@phosphor-icons/react";
 import {useModal} from "../../context/ModalContext.jsx";
 
-export default function AuthenticatedNav() {
+export default function AuthenticatedNav({ onProjectCreated }) {
     const { openModal } = useModal();
 
     const handleNewProject = () => {
-        openModal("createProject");
+        openModal("createProject", {
+            onProjectCreated
+        });
     }
 
     return (
--- frontend/src/pages/Dashboard/Dashboard.jsx
+++ frontend/src/pages/Dashboard/Dashboard.jsx
@@ -41,7 +41,7 @@ export default function Dashboard() {
 
     return (
         <div className={styles["dashboard-wrapper"]}>
-            <AuthenticatedNav />
+            <AuthenticatedNav onProjectCreated={handleProjectCreated}/>
             <SideNavWrapper>
                 <HeaderControls onProjectCreated={handleProjectCreated} />
                 <ProjectGrid
